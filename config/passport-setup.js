62d8e2d9926dd9ecb8cfb38c089460c5a19a99f2
Poonam <poo17nam@gmail.com>
2018-04-28 21:21:34 +0530

    lookup/create user record in db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,34 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
+
 passport.use(
   new GoogleStrategy({
-  // options for the google strategy
-  callbackURL: '/auth/google/redirect',
-  clientID : keys.google.clientID,
-  clientSecret: keys.google.clientSecret
-}, (accessToken, refreshToken, profile, done) => {
-  // passport callback function
-  console.log('passport callback function fired');
-  console.log(profile);
-}))
+    // options for the google strategy
+    callbackURL: '/auth/google/redirect',
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+  }, (accessToken, refreshToken, profile, done) => {
+    // check if user already exists in our db
+
+    console.log('User:', User);
+    User.findOne({
+      googleId: profile.id
+    }).then((currentUser) => {
+      if (currentUser) {
+        //already have the user
+        console.log('user is :',currentUser);
+      } else {
+        // if not create user in our db
+        new User({
+          username: profile.displayName,
+          googleId: profile.id
+        }).save().then((newUser) => {
+          console.log('new user created:', newUser);
+        })
+      }
+    })
+
+  })
+)
